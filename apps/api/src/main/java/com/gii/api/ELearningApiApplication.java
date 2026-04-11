eba2e3e949a6ca153d0951d8763b0afaf6086674
RajibAlam43 <rajibalam202@gmail.com>
2026-04-10 23:20:01 -0500

    feat: add multi modules config for api and worker

---
+++ apps/api/src/main/java/com/gii/api/ELearningApiApplication.java
@@ -0,0 +1,14 @@
+package com.gii.apps.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ELearningApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ELearningApiApplication.class, args);
+	}
+
+}
+
