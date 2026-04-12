a9f0a0e8a10fe3e50615b7740be1290c2151a4e1
RajibAlam43 <72268622+RajibAlam43@users.noreply.github.com>
2026-04-11 23:41:12 -0500

    feat: infra setup (#3)

--- apps/worker/src/main/java/com/gii/worker/ELearningWorkerApplication.java
+++ apps/worker/src/main/java/com/gii/worker/ELearningWorkerApplication.java
@@ -1,9 +1,15 @@
-package com.gii.apps.worker;
+package com.gii.worker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@EnableScheduling
+@SpringBootApplication(scanBasePackages = {"com.gii.worker", "com.gii.common"})
+@EntityScan(basePackages = {"com.gii.common.model"})
+@EnableJpaRepositories(basePackages = {"com.gii.common.repository"})
 public class ELearningWorkerApplication {
 
 	public static void main(String[] args) {
