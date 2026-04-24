c6830803f11f3d14d055688353389f4412c2a7d3
RajibAlam43 <rajibalam202@gmail.com>
2026-04-24 11:54:05 -0500

    chore: more cleanup

--- apps/worker/src/main/java/com/gii/worker/ELearningWorkerApplication.java
+++ apps/worker/src/main/java/com/gii/worker/ELearningWorkerApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
 @SpringBootApplication(scanBasePackages = {"com.gii.worker", "com.gii.common"})
 @EntityScan(basePackages = {"com.gii.common.model"})
 @EnableJpaRepositories(basePackages = {"com.gii.common.repository"})
